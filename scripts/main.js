b0187d4d20278672f89f50830973c979628eae4e
ModerTry <90960967+ModerTry@users.noreply.github.com>
2021-09-18 22:12:23 +0530

    Modern Folder Uploaded

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function toggleForm(){
+    section =document.querySelector('.section');
+    contain =document.querySelector('.contain');
+    contain.classList.toggle('active');
+    section.classList.toggle('active');
+    section =document.querySelector('.section');
+    container =document.querySelector('.container');
+    container.classList.toggle('active');
+    section.classList.toggle('active');
+   
+}
+
+
+
+
+
+
+
+
+
+
+
